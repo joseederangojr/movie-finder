6d60822a8fc229a4ce4c04f9bb12f9234128f4ac
Jose Ederango Jr <joseederangojr@gmail.com>
2025-04-26 11:29:30 +0800

    add configuration for redis tls

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,33 +1,35 @@
-import { createClient } from "redis"
+import { createClient } from "redis";
 
 class Cache {
-  private client: ReturnType<typeof createClient>
+	private client: ReturnType<typeof createClient>;
 
-  constructor() {
-    this.client = createClient({
-      url: process.env.REDIS_URL || "redis://localhost:6379",
-    })
+	constructor() {
+		this.client = createClient({
+			url: process.env.REDIS_URL || "redis://localhost:6379",
+			socket: {
+				tls: process.env.REDIS_TLS === "1",
+			},
+		});
 
-    this.client.on("error", (err) => console.error("Redis Client Error", err))
-  }
+		this.client.on("error", (err) => console.error("Redis Client Error", err));
+	}
 
-  private async connect() {
-    if (!this.client.isOpen) {
-      await this.client.connect()
-    }
-  }
+	private async connect() {
+		if (!this.client.isOpen) {
+			await this.client.connect();
+		}
+	}
 
-  async get(key: string): Promise<string | null> {
-    await this.connect()
-    return this.client.get(key)
-  }
+	async get(key: string): Promise<string | null> {
+		await this.connect();
+		return this.client.get(key);
+	}
 
-  async set(key: string, value: string, ttl: number): Promise<void> {
-    await this.connect()
-    await this.client.setEx(key, ttl, value)
-  }
+	async set(key: string, value: string, ttl: number): Promise<void> {
+		await this.connect();
+		await this.client.setEx(key, ttl, value);
+	}
 }
 
-const cache = new Cache()
-export default cache
-
+const cache = new Cache();
+export default cache;
